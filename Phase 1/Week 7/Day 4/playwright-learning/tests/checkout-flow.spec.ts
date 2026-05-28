36eae34c42c69e3c4e392307b8a4d18a11406452
Bedi Gupta <bedigupta76@gmail.com>
2026-05-28 21:43:26 +0530

    sort products by price low to high and verify

--- Phase 1/Week 7/Day 4/playwright-learning/tests/checkout-flow.spec.ts
+++ Phase 1/Week 7/Day 4/playwright-learning/tests/checkout-flow.spec.ts
@@ -13,32 +13,42 @@ test.describe("SauceDemo Complete Checkout flow", () => {
 
   test("Add single product to cart and verify cart count", async ({ page }) => {
     // Product selection and adding to cart
-    await page.locator(".inventory_list > .inventory_item").filter({ hasText: "Sauce Labs Backpack" }).getByRole("button", { name: "Add to cart" }).click();
+    await page
+      .locator(".inventory_list > .inventory_item")
+      .filter({ hasText: "Sauce Labs Backpack" })
+      .getByRole("button", { name: "Add to cart" })
+      .click();
     // Verify Cart Count
     await expect(page.locator(".shopping_cart_link > span")).toHaveText("1");
   });
 
-  test('add multiple prducts and verify cart',async({page})=>{
+  test("add multiple prducts and verify cart", async ({ page }) => {
     // Add Multiple Products
-    const fetchProduct = page.locator('.inventory_list > .inventory_item');
+    const fetchProduct = page.locator(".inventory_list > .inventory_item");
     const productCount = await fetchProduct.count();
-    for (let i = 0; i < productCount;i++){
-      const product = fetchProduct.nth(i).getByRole('button',{name: 'Add to cart'});
+    for (let i = 0; i < productCount; i++) {
+      const product = fetchProduct
+        .nth(i)
+        .getByRole("button", { name: "Add to cart" });
       await product.click();
     }
     await expect(page.locator(".shopping_cart_link > span")).toHaveText("6");
-  })
-  test('sort products by price low to high',async({page})=>{
-    const sortDropdown = page.locator('.product_sort_container');
-    await sortDropdown.selectOption({label:'Price (low to high)'})
+  });
+  test("sort products by price low to high", async ({ page }) => {
+    const sortDropdown = page.locator(".product_sort_container");
+    await sortDropdown.selectOption({ label: "Price (low to high)" });
     // In this command I am getting all the prices of the products from ascending order and storing them in an array
-    const inventoryPrice = await page.locator('.inventory_item_price').allInnerTexts();
+    const inventoryPrice = await page
+      .locator(".inventory_item_price")
+      .allInnerTexts();
     // Take each value remove dollar sign and convert string into number
-    const priceArray = inventoryPrice.map(price => parseFloat(price.replace('$', '')));
+    const priceArray = inventoryPrice.map((price) =>
+      parseFloat(price.replace("$", "")),
+    );
     // check is it in the ascending order or not
     const checkValues = priceArray.every((price, index) => {
       return index === 0 || price >= priceArray[index - 1];
     });
     expect(checkValues).toBeTruthy();
-    })
+  });
 });
