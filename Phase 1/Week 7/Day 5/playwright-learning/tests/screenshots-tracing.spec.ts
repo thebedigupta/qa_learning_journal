73ccdfd300c5bd3eab51e94c5be6689dd6d4850c
Bedi Gupta <bedigupta76@gmail.com>
2026-06-01 10:00:50 +0530

    feat: learn to see traceviwer and problem casued by it

--- Phase 1/Week 7/Day 5/playwright-learning/tests/screenshots-tracing.spec.ts
+++ Phase 1/Week 7/Day 5/playwright-learning/tests/screenshots-tracing.spec.ts
@@ -1,81 +1,92 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
+test.describe("Screenshot and Tracing Practice", () => {
+  test("take manual screenshot at each checkout step", async ({ page }) => {
+    await page.goto("https://saucedemo.com/");
 
-test.describe('Screenshot and Tracing Practice',()=>{
+    //Screenshot 1 - login page
 
-    test('take manual screenshot at each checkout step',async ({page})=>{
-        await page.goto('https://saucedemo.com/')
+    await page.screenshot({
+      path: "screenshots/01-login-page.png",
+      fullPage: true,
+    });
 
-        //Screenshot 1 - login page
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
 
-        await page.screenshot({
-            path: 'screenshot/01-login-page.png',
-            fullPage: true
-        });
+    // Screenshot 2 - Inventory Page
+    await page.screenshot({
+      path: "screenshots/02-inventory-page.png",
+      fullPage: true,
+    });
 
-        await page.getByPlaceholder('Username').fill('standard_user');
-        await page.getByPlaceholder('Password').fill('secret_sauce');
-        await page.getByRole('button',{name: 'Login'}).click()
+    await page.getByRole("button", { name: "Add to cart" }).first().click();
+    await page.locator(".shopping_cart_badge").click();
 
-        // Screenshot 2 - Inventory Page
-        await page.screenshot({
-            path: 'screenshot/02-inventory-page.png',
-            fullPage: true
-        })
+    //Screenshot -3 Checkout Page
+    await page.screenshot({
+      path: "screenshots/03-Checkout-page.png",
+      fullPage: true,
+    });
 
-        await page.getByRole('button',{name: 'Add to cart'}).first().click()
-        await page.locator('.shopping_cart_badge').click();
+    await page.getByRole("button", { name: "Checkout" }).click();
+    await page.getByPlaceholder("First Name").fill("Bedi");
+    await page.getByPlaceholder("Last Name").fill("Gupta");
+    await page.getByPlaceholder("Zip/Postal Code").fill("274001");
+    await page.getByRole("button", { name: "Continue" }).click();
 
-        //Screenshot -3 Checkout Page
-        await page.screenshot({
-            path: 'screenshot/03-Checkout-page.png',
-            fullPage: true
-        })
+    // Order overview
+    await page.screenshot({
+      path: "screenshots/04-Order-overview.png",
+      fullPage: true,
+    });
+    await page.getByRole("button", { name: "Finish" }).click();
 
-        await page.getByRole('button',{name: 'Checkout'}).click();
-        await page.getByPlaceholder('First Name').fill('Bedi');
-        await page.getByPlaceholder('Last Name').fill('Gupta');
-        await page.getByPlaceholder('Zip/Postal Code').fill('274001');
-        await page.getByRole('button',{name:'Continue'}).click();
+    // Screenshot - Order Complete
+    await page.screenshot({
+      path: "screenshots/05-order-complete.png",
+      fullPage: true,
+    });
 
-        // Order overview
-        await page.screenshot({
-            path:'screenshot/04-Order-overview.png',
-            fullPage: true,
-        })
-        await page.getByRole('button',{name: 'Finish'}).click();
+    await expect(
+      page.getByRole("heading", { name: "Thank you for your order!" }),
+    ).toBeVisible();
+  });
 
-        // Screenshot - Order Complete
-        await page.screenshot({
-            path: 'screenshot/05-order-complete.png',
-            fullPage : true
-        })
-        
-        await expect(page.getByRole('heading',{name :'Thank you for your order!' })).toBeVisible();
-    })
-    
-})
-
-test('take element screenshot of product card',async({page})=>{
-    await page.goto('https://saucedemo.com');
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.getByRole('button',{name:'Login'}).click();
+  test("take element screenshot of product card", async ({ page }) => {
+    await page.goto("https://saucedemo.com");
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
 
     // First Product Card item
-    await page.locator('.inventory_item').first().screenshot({
-        path: 'screenshot/01-first-product.png',
-    })
+    await page.locator(".inventory_item").first().screenshot({
+      path: "screenshots/01-first-product.png",
+    });
 
-    await expect(page.locator('inventory_item').first()).toBeVisible();
-})
+    await expect(page.locator(".inventory_item").first()).toBeVisible();
+  });
 
-//   test('snapshot test — login page visual regression', async ({ page }) => {
-//     await page.goto('https://www.saucedemo.com');
+  test("snapshot test — login page visual regression", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com");
 
-//     // First run saves reference
-//     // Subsequent runs compare
-//     await expect(page).toHaveScreenshot('saucedemo-login.png', {
-//       maxDiffPixels: 200
-//     });
-//   });
+    // First run saves reference
+    // Subsequent runs compare
+    // The Modern SDET Standard for Visual Testing
+    await expect(page).toHaveScreenshot("saucedemo-login.png", {
+      maxDiffPixels: 200,
+    });
+  });
+
+  test("Broken - intentional faliure to practice trace viewer", async ({
+    page,
+  }) => {
+    await page.goto("https://www.saucedemo.com");
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator('.inventory_item')).toHaveCount(10);
+  });
+});
