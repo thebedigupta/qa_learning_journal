e3c70c01de38c046252d5916164ae51ee7737d54
Bedi Gupta <bedigupta76@gmail.com>
2026-06-02 21:40:34 +0530

    feat: file upload handle &  dialog alert

--- Phase 1/Week 7/Day 6/playwright-learning/tests/advanced-elements.spec.ts
+++ Phase 1/Week 7/Day 6/playwright-learning/tests/advanced-elements.spec.ts
@@ -1,22 +1,37 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Advanced element practice',()=>{
+test.describe("Advanced element practice", () => {
+  test("upload file on contact page", async ({ page }) => {
+    await page.goto("https://automationexercise.com/contact_us");
 
-    test('handle dialog alert on automation exercise',async({page})=>{
-        await page.goto('https://automationexercise.com/contact_us');
+    await page.getByPlaceholder("Name").fill("Ajay");
+    await page.locator('[data-qa="email"]').fill("test@example.com");
+    await page.getByPlaceholder("Subject").fill("Test Subject");
+    await page.getByPlaceholder("Your Message Here").fill("Test Message");
 
-        await page.getByPlaceholder('Name').fill('Ajay');
-        await page.locator('[data-qa="email"]').fill('test@example.com');
-        await page.getByPlaceholder('Subject').fill('Test Subject');
-        await page.getByPlaceholder('Your Message Here').fill('Test Message');
-        page.once('dialog',dialog=> dialog.accept())
-        const filePath = page.locator('input[type="file"]');
-        await filePath.setInputFiles({
-          name : 'text.txt',
-          mimeType:'text/plain',
-          buffer:Buffer.from('Test file content')
-        })
-        await expect(filePath).toBeAttached();
-        await page.getByRole('button',{name :'Submit',exact:true}).click();
-    })
-})
+    const filePath = page.locator('input[type="file"]');
+    await filePath.setInputFiles({
+      name: "text.txt",
+      mimeType: "text/plain",
+      buffer: Buffer.from("Test file content"),
+    });
+    await expect(filePath).toBeAttached();
+    // single use pop up and then destory
+    page.once("dialog", (dialog) => dialog.accept());
+    await page.getByRole("button", { name: "Submit", exact: true }).click();
+    await expect(page.locator(".contact-form .alert-success")).toBeVisible();
+  });
+
+  test('handle dialog alert on automation exercise',async({page})=>{
+    await page.goto("https://automationexercise.com/contact_us")
+
+    await page.getByPlaceholder('Name').fill('Ravi');
+    await page.getByRole('textbox',{name : 'Email',exact:true}).fill('test@test.com');
+    await page.getByPlaceholder('Subject').fill('Test Subject Exercise');
+    await page.getByPlaceholder('Your Message Here').fill('Test message');
+    // Single use pop up and then destory
+    page.once('dialog',(dialog)=>{dialog.accept()});
+    await page.getByRole('button',{name : 'Submit'}).click()
+    await expect(page.locator('.contact-form .alert-success')).toBeVisible();
+  })
+});
