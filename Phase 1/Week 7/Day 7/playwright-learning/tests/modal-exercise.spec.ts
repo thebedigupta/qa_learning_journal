2d8acbe72c4c47fd9a62abfdf779f1c23c0c46a4
Bedi Gupta <bedigupta76@gmail.com>
2026-06-04 08:59:56 +0530

    feat: Add multiple products via model flow

--- Phase 1/Week 7/Day 7/playwright-learning/tests/modal-exercise.spec.ts
+++ Phase 1/Week 7/Day 7/playwright-learning/tests/modal-exercise.spec.ts
@@ -11,4 +11,23 @@ test.describe("Modal handling on Automation Exercise", () => {
     await modelBox.getByRole("button", { name: "Continue Shopping" }).click();
     await expect(modelBox).not.toBeVisible();
   });
+  test("Add multiple prosucts via model flow", async ({ page }) => {
+    await page.goto("https://automationexercise.com/");
+
+    const modelPopup = page.locator(".modal-content");
+    const productOne = page.locator(".productinfo").nth(0);
+    await productOne.hover();
+    await productOne.getByText("Add to cart").click();
+    await expect(modelPopup).toBeVisible();
+    await expect(modelPopup).toContainText("Added");
+    await modelPopup.getByRole("button", { name: "Continue Shopping" }).click();
+    await expect(modelPopup).not.toBeVisible();
+    const productTwo = page.locator(".productinfo").nth(1);
+    await productTwo.hover();
+    await productTwo.getByText("Add to cart").click();
+    await expect(modelPopup).toBeVisible();
+    await expect(modelPopup).toContainText("Added");
+    await modelPopup.getByRole("link", { name: "View Cart" }).click();
+    await expect(page).toHaveURL(/.*view_cart/);
+  });
 });
