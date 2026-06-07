5ecd9e6e814bc37036d7ffd7cb9e83a4700266d4
Bedi Gupta <bedigupta76@gmail.com>
2026-06-07 11:36:51 +0530

    feat: Login page completed

--- Phase 1/Week 8/Day 3/playwright-pom/pages/BasePage.ts
+++ Phase 1/Week 8/Day 3/playwright-pom/pages/BasePage.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
 
-export class Basepage {
+export class BasePage {
   constructor(protected page: Page) {}
 
   async await() {
---
+++ Phase 1/Week 8/Day 3/playwright-pom/pages/LoginPage.ts
@@ -0,0 +1,40 @@
+import {Page,expect} from '@playwright/test';
+
+import {BasePage} from './BasePage';
+
+export class LoginPage extends BasePage {
+
+    private readonly usernameInput = this.page.locator('[data-test="username"]');
+    private readonly passwordInput = this.page.locator('[data-test="password"]');
+    private readonly loginButton = this.page.locator('[data-test="login-button"]');
+    private readonly errorMessage = this.page.locator('[data-test="error"]');
+
+    constructor(page:Page){
+        super(page)
+    }
+
+    async goto(){
+        await this.page.goto('/')
+    }
+
+    async login (username :string ,password :string){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password)
+        await this.loginButton.click()
+    }
+    
+    // ----------- Assertions ------------
+
+    async assertLoginSucess(){
+        await expect(this.page).toHaveURL('/inventory.html');
+    }
+
+    async assertErrorMessage(expectedMessage:string){
+        await expect(this.errorMessage).toBeVisible();
+        await expect(this.errorMessage).toContainText(expectedMessage);
+    }
+    
+    async assertLoginPage(){
+        await expect(this.loginButton).toBeVisible();
+    }
+}
