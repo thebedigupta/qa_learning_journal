f733a7ead884d7745ee826077ec4d7751fa030ff
Bedi Gupta <bedigupta76@gmail.com>
2026-06-08 07:43:01 +0530

    feat: fix few issue in checkout page

--- Phase 1/Week 8/Day 3/playwright-pom/pages/CheckoutPage.ts
+++ Phase 1/Week 8/Day 3/playwright-pom/pages/CheckoutPage.ts
@@ -10,61 +10,66 @@ export class CheckoutPage extends BasePage {
   private readonly errorShown = this.page.locator('[data-test="error"]');
   private readonly checkoutOverview = this.page.locator('[data-test="title"]');
   private readonly shoppingFinish = this.page.locator('[data-test="finish"]');
-  private readonly continueShopping = this.page.locator('[data-test="continue"]');
+  private readonly continueShopping = this.page.locator(
+    '[data-test="continue"]',
+  );
   private readonly checkoutButton = this.page.locator('[data-test="checkout"]');
-  private readonly summaryLabel = this.page.locator('[data-test="total-info-label"]')
+  private readonly summaryLabel = this.page.locator(
+    '[data-test="total-info-label"]',
+  );
 
-  constructor(page:Page){
+  constructor(page: Page) {
     super(page);
   }
 
-  async fillShippingInfo(expetcedFName:string,expectedLName:string,expectedZip:string){
+  async fillShippingInfo(
+    expetcedFName: string,
+    expectedLName: string,
+    expectedZip: string,
+  ) {
     await this.firsNameInput.fill(expetcedFName);
     await this.lastNameInput.fill(expectedLName);
     await this.postalCodeinput.fill(expectedZip);
-    await this.continueShopping.click()
+    await this.continueShopping.click();
   }
 
   // Click Finish On the order summary screen finish order
 
-  async finishOrder(){
+  async finishOrder() {
     await this.shoppingFinish.click();
   }
 
   // Go Back to product page from the sucess screen
 
-  async backToProduct(){
+  async backToProduct() {
     await this.continueShopping.click();
   }
 
   // ----------------- Assertion --------------------
 
   // Assert we landed on Step 1 (form is visible)
-  async OnCheckoutStepOne(){
+  async OnCheckoutStepOne() {
     await expect(this.firsNameInput).toBeVisible();
   }
 
   // Assert we landed on step 2(Order Summary)
 
-  async assertOnCheckoutStepTwo(){
+  async assertOnCheckoutStepTwo() {
     await expect(this.checkoutOverview).toContainText(/Overview/i);
   }
-  // Assert Order was placed sucessfully 
-  async assertOrderStatus(){
+  // Assert Order was placed sucessfully
+  async assertOrderStatus() {
     await expect(this.checkoutOverview).toContainText(/Complete!/i);
   }
 
   // Assert error shown when required field is missing
-  async asserFormError(expectedText:string){
+  async asserFormError(expectedText: string) {
     await expect(this.errorShown).toBeVisible();
     await expect(this.errorShown).toContainText(expectedText);
   }
 
   // Assert the item total label is visible (we're on overview screen)
-
-  async assertItemTotalVisible(){
+  async assertItemTotalVisible() {
     await expect(this.summaryLabel).toBeVisible();
   }
-
-
 }
