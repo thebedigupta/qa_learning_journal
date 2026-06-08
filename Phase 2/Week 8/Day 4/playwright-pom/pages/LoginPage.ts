416b12a20e5ab3e3ebc8a6db23af18431d3aaa2b
Bedi Gupta <bedigupta76@gmail.com>
2026-06-08 19:29:47 +0530

    feat: added one assertion in loginPage

--- Phase 2/Week 8/Day 4/playwright-pom/pages/LoginPage.ts
+++ Phase 2/Week 8/Day 4/playwright-pom/pages/LoginPage.ts
@@ -1,40 +1,46 @@
-import {Page,expect} from '@playwright/test';
+import { Page, expect } from "@playwright/test";
 
-import {BasePage} from './BasePage';
+import { BasePage } from "./BasePage";
 
 export class LoginPage extends BasePage {
+  private readonly usernameInput = this.page.locator('[data-test="username"]');
+  private readonly passwordInput = this.page.locator('[data-test="password"]');
+  private readonly loginButton = this.page.locator(
+    '[data-test="login-button"]',
+  );
+  private readonly errorMessage = this.page.locator('[data-test="error"]');
 
-    private readonly usernameInput = this.page.locator('[data-test="username"]');
-    private readonly passwordInput = this.page.locator('[data-test="password"]');
-    private readonly loginButton = this.page.locator('[data-test="login-button"]');
-    private readonly errorMessage = this.page.locator('[data-test="error"]');
+  constructor(page: Page) {
+    super(page);
+  }
 
-    constructor(page:Page){
-        super(page)
-    }
+  async goto() {
+    await this.page.goto("/");
+  }
 
-    async goto(){
-        await this.page.goto('/')
-    }
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
 
-    async login (username :string ,password :string){
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password)
-        await this.loginButton.click()
-    }
-    
-    // ----------- Assertions ------------
+  // ----------- Assertions ------------
 
-    async assertLoginSucess(){
-        await expect(this.page).toHaveURL('/inventory.html');
-    }
+  async assertLoginSucess() {
+    await expect(this.page).toHaveURL("/inventory.html");
+  }
 
-    async assertErrorMessage(expectedMessage:string){
-        await expect(this.errorMessage).toBeVisible();
-        await expect(this.errorMessage).toContainText(expectedMessage);
-    }
-    
-    async assertLoginPage(){
-        await expect(this.loginButton).toBeVisible();
-    }
+  async assertErrorMessage(expectedMessage: string) {
+    await expect(this.errorMessage).toBeVisible();
+    await expect(this.errorMessage).toContainText(expectedMessage);
+  }
+
+  async assertLoginPage() {
+    await expect(this.loginButton).toBeVisible();
+  }
+
+  // Add this method to LoginPage.ts
+  async assertErrorVisible() {
+    await expect(this.errorMessage).toBeVisible();
+  }
 }
