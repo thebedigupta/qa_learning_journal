09aaaffbc4b2a2d970dd772381eef725a9fd0a0a
Bedi Gupta <bedigupta76@gmail.com>
2026-06-09 21:32:01 +0530

    feat: completed go to cart  page fixtures

--- Phase 2/Week 8/Day 5/playwright-pom/fixtures/customFixtures.ts
+++ Phase 2/Week 8/Day 5/playwright-pom/fixtures/customFixtures.ts
@@ -2,10 +2,13 @@ import {test as base,expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
 import {CartPage} from '../pages/CartPage';
 import {ProductPage} from '../pages/ProductPage';
-import {users} from '../utils/testData';
+import {CheckoutPage} from '../pages/CheckoutPage';
+import {users,products} from '../utils/testData';
 
 type customFixtures = {
-    loggedInPage : ProductPage
+    loggedInPage : ProductPage;
+    cartPage : CartPage;
+    productPage : CheckoutPage
 
 }
 export const test = base.extend<customFixtures>({
@@ -27,5 +30,20 @@ export const test = base.extend<customFixtures>({
         await use(productPage);
 
         // TearDown : nothing needed playwright handles page cleaup
+    },
+
+    cartPage: async({page},use)=>{
+        const loginPage = new LoginPage(page);
+        
+        await loginPage.goto();
+        await loginPage.login(users.standard.username,users.standard.password);
+        await loginPage.assertLoginSucess();
+
+        const productPage = new ProductPage(page);
+        await productPage.addProductsToCart(products.backpack);
+        await productPage.goToCart();
+        
+        const cartPage = new CartPage(page);
+        await use(cartPage);
     }
 });
