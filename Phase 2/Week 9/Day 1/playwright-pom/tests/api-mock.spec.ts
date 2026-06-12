49de011db6c0a318a0260f0a5acc36468e6dbeb4
Bedi Gupta <bedigupta76@gmail.com>
2026-06-12 14:19:18 +0530

    feat: modify real products API and inject extra item

--- Phase 2/Week 9/Day 1/playwright-pom/tests/api-mock.spec.ts
+++ Phase 2/Week 9/Day 1/playwright-pom/tests/api-mock.spec.ts
@@ -14,4 +14,15 @@ test("Mock API Completely", async ({ page }) => {
   await expect(page.getByText("Dragon Fruit")).toBeVisible();
 });
 
+test("Mock API Partially", async ({ page }) => {
+  await page.route("*/**/api/v1/fruits", async (route) => {
+    const response = await route.fetch();
+    const json = await response.json();
+    json.push({ name: "Grapes fruits", id: 999 });
+    await route.fulfill({ response, json });
+  });
+  await page.goto("https://demo.playwright.dev/api-mocking");
+  await expect(page.getByText("Grapes fruits")).toBeVisible();
+});
+
 
