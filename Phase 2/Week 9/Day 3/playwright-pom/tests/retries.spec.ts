a9cb11b5ebd3b7913d6c6b3e6ef3e073b4187d09
Bedi Gupta <bedigupta76@gmail.com>
2026-06-15 09:27:00 +0530

    feat: time based falkyness with wait

--- Phase 2/Week 9/Day 3/playwright-pom/tests/retries.spec.ts
+++ Phase 2/Week 9/Day 3/playwright-pom/tests/retries.spec.ts
@@ -19,3 +19,18 @@ test("simulates falky test failed once then passed", async ({
   await expect(page).toHaveTitle(/swag labs/i);
 });
 
+test("timing based flakyness - retry with wait", async ({ page }, testInfo) => {
+  attemptCount++;
+
+  await page.goto("https://www.saucedemo.com");
+
+  if (testInfo.retry === 0) {
+    const title = await page.title();
+    if (title !== "Swag Labs!!!") {
+      throw new Error("Title is mismatch");
+    }
+  }
+
+  await expect(page).toHaveTitle(/Swag Labs/i);
+  console.log(`Passed on attempt #${testInfo.retry + 1}`);
+});
