2fd6264e6343b28580a6cd6f6389023ca4ca4c29
Bedi Gupta <bedigupta76@gmail.com>
2026-06-16 19:13:08 +0530

    feat: HomePage setup complete

---
+++ Phase 2/Week 9/Day 4/ae-automation/pages/HomePage.ts
@@ -0,0 +1,42 @@
+import { Page, expect } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+export class HomePage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async open(): Promise<void> {
+    await this.init();
+    await this.navigate();
+  }
+
+  async verifyHomePageVisible(): Promise<void> {
+    await expect(this.page).toHaveURL(/automationexercise/);
+    await expect(
+      this.page.locator('img[alt="Website for practice automation"]'),
+    ).toBeVisible();
+  }
+
+  async scorlltofooter(): Promise<void> {
+    await this.page.evaluate(() =>
+      window.scrollTo(0, document.body.scrollHeight),
+    );
+  }
+
+  async scorlltoTop(): Promise<void> {
+    await this.page.evaluate(() => window.scrollTo(0, 0));
+  }
+
+  async subscribeWithEmail(email: string): Promise<void> {
+    await this.page.evaluate(() =>
+      window.scrollTo(0, document.body.scrollHeight),
+    );
+    await expect(this.page.getByText("SUBSCRIPTION")).toBeVisible();
+    await this.page.fill("#susbscribe_email", `${email}`);
+    await this.page.click("#subscribe");
+  }
+  async clickArrowUpButton(): Promise<void> {
+    await this.page.click("#scrollUp");
+  }
+}
