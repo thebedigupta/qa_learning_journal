1217b08bb5dd181fcf3c4ba120ef121b3f06705f
Bedi Gupta <bedigupta76@gmail.com>
2026-06-23 19:47:51 +0530

    refactor(products): optimize test structure via explicit deep-linking and clean DDT grouping

--- Phase 2/Week 9/Day 4/ae-automation/tests/tc08-09-products.spec.ts
+++ Phase 2/Week 9/Day 4/ae-automation/tests/tc08-09-products.spec.ts
@@ -4,29 +4,31 @@ import { ProductDetailsPage } from "../pages/ProductDetailsPage";
 import { searchTerms } from "../utils/testData";
 
 test.describe("Test all products are visible with search multiple terms and product details", () => {
-  test.beforeEach(async ({ page }) => {
-    const productPage = new ProductPage(page);
-    await productPage.open();
-    await productPage.verifyAllProductsVisible();
-  });
-
   // TC8 - View all products and product detail page
 
   test("TC8 - View all products and product detail page", async ({ page }) => {
     const productPage = new ProductPage(page);
+    const productDetailsPage = new ProductDetailsPage(page);
+
+    await productPage.open();
+    await productPage.verifyAllProductsVisible();
     await productPage.hoverAndAddToCart(0);
     await productPage.viewCartFromModal();
-    const productDetailsPage = new ProductDetailsPage(page);
     await productDetailsPage.verifyProductsDetailsPage();
   });
 
   // TC9 - Search Products with different keywords
-
-  for (let term of searchTerms) {
-    test(`TC9 - Search Products with term ${term.trim()}`, async ({ page }) => {
-      const productPage = new ProductPage(page);
-      await productPage.searchProduct(term.trim());
-      await productPage.verifySearchResultVisible();
-    });
-  }
+  test.describe("", () => {
+    for (let term of searchTerms) {
+      test(`TC9 - Search Products with term ${term.trim()}`, async ({
+        page,
+      }) => {
+        const productPage = new ProductPage(page);
+        await productPage.open();
+        await productPage.verifyAllProductsVisible();
+        await productPage.searchProduct(term.trim());
+        await productPage.verifySearchResultVisible();
+      });
+    }
+  });
 });
