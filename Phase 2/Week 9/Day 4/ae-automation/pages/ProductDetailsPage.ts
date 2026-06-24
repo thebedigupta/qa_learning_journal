707b28a4a0c3253eea64d0cefb1464f4becb4331
Bedi Gupta <bedigupta76@gmail.com>
2026-06-24 20:31:11 +0530

    Add two products into cart and fix flaky cart quantity test by adding explicit waits

--- Phase 2/Week 9/Day 4/ae-automation/pages/ProductDetailsPage.ts
+++ Phase 2/Week 9/Day 4/ae-automation/pages/ProductDetailsPage.ts
@@ -6,25 +6,23 @@ export class ProductDetailsPage extends BasePage {
   private readonly addProductToCart = this.page.getByRole("button", {
     name: "Add to cart",
   });
-  private readonly viewCartProductsModalBtn = this.page.locator(
-    ".modal-content .text-center",
-  );
   private readonly reviewNameInput = this.page.locator("#name");
   private readonly reviewEmailInput = this.page.locator("#email");
   private readonly reviewMessageBox = this.page.locator("#review");
   private readonly reviewButton = this.page.locator("#button-review");
-  private readonly checkoutBtn = this.page.locator('.container .check_out'); 
-  private readonly itemTable = this.page.locator('#cart_info_table');
-  private readonly shoppingCartTitle = this.page.locator('.breadcrumbs .active');
+  private readonly viewCartModalLink = this.page.locator(".modal-content");
 
   constructor(page: Page) {
     super(page);
   }
 
   async verifyProductsDetailsPage(): Promise<void> {
-    await expect(this.itemTable).toBeVisible();
-    await expect(this.shoppingCartTitle).toHaveText(/Shopping Cart/i);
-    await expect(this.checkoutBtn).toHaveText(/Proceed To Checkout/i);
+    await expect(this.page.locator(".product-information")).toBeVisible();
+    await expect(
+      this.page
+        .locator(".product-information")
+        .getByRole("button", { name: "Add to cart" }),
+    ).toBeVisible();
   }
 
   async setQuantity(qty: number): Promise<void> {
@@ -32,11 +30,17 @@ export class ProductDetailsPage extends BasePage {
   }
 
   async addToCart(): Promise<void> {
-    await this.addProductToCart.click();
+    await expect(async () => {
+      await this.addProductToCart.click();
+      await expect(
+        this.viewCartModalLink.locator('a[href="/view_cart"]'),
+      ).toBeVisible({ timeout: 2000 });
+    }).toPass();
   }
 
   async viewCart(): Promise<void> {
-    await this.viewCartProductsModalBtn.locator('a[href="/view_cart"]').click();
+    // Wait for the modal link text specifically to pop up on screen
+    await this.viewCartModalLink.locator('a[href="/view_cart"]').click();
   }
 
   async fillreview(
