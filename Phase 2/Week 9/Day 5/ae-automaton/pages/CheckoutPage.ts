7e62627d27b38871ef8045f3d4a581b2b592c8a3
Bedi Gupta <bedigupta76@gmail.com>
2026-07-01 09:32:12 +0530

    feat: created checkoutpage complete

--- Phase 2/Week 9/Day 5/ae-automaton/pages/CheckoutPage.ts
+++ Phase 2/Week 9/Day 5/ae-automaton/pages/CheckoutPage.ts
@@ -2,22 +2,41 @@ import { Page, expect } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class CheckoutPage extends BasePage {
-    private readonly firstProductImageVisible = this.page.locator('#product-1').getByRole('link', { name: 'Product Image' });
-    private readonly registerLoginModalPopup = this.page.getByText('Checkout Register / Login');
-    private readonly clickRegisterLoginBtn = this.page.getByRole('link',{name:'Register/Login'});
+  private readonly firstProductImageVisible = this.page
+    .locator("#product-1")
+    .getByRole("link", { name: "Product Image" });
+  private readonly registerLoginModalPopup = this.page.getByText(
+    "Checkout Register / Login",
+  );
+  private readonly clickRegisterLoginBtn = this.page.getByRole("link", {
+    name: "Register/Login",
+  });
   constructor(page: Page) {
     super(page);
   }
 
-  async verifyCheckoutPage():Promise<void>{
+  async verifyCheckoutPage(): Promise<void> {
     await expect(this.page).toHaveURL(/checkout/i);
     await expect(this.firstProductImageVisible).toBeVisible();
   }
 
-  async clickRegisterLoginFromModal():Promise<void>{
+  async clickRegisterLoginFromModal(): Promise<void> {
     await expect(this.registerLoginModalPopup).toBeVisible();
-    await this.clickRegisterLoginBtn.click(); 
+    await this.clickRegisterLoginBtn.click();
   }
 
-  
+  async verifyDeliveryAddress(address: string,country:string): Promise<void> {
+    await expect(this.page.locator('#address_delivery').getByText(address)).toBeVisible();
+    await expect(this.page.locator('#address_delivery').getByText(country)).toBeVisible();
+  }
+
+  async verifyBillingAddress(address:string,country:string){
+    await expect(this.page.locator('#address_invoice').getByText(address)).toBeVisible();
+    await expect(this.page.locator('#address_invoice').getByText(country)).toBeVisible();
+  }
+
+  async enterCommentAndPlaceOrder(comment:string):Promise<void>{
+    await this.page.locator('textarea.form-control').fill(comment);
+    await this.page.getByRole('link',{name:'Place Order',exact:true}).click();
+  }
 }
