c9dfe2f6a4c226fca26ff09288f191ebf90f29b2
Bedi Gupta <bedigupta76@gmail.com>
2026-07-07 00:54:30 +0530

    TC21 Completed and passed

--- Phase 2/Week 9/Day 5/ae-automaton/tests/tc20-22-products-advanced.spec.ts
+++ Phase 2/Week 9/Day 5/ae-automaton/tests/tc20-22-products-advanced.spec.ts
@@ -1,34 +1,94 @@
-import {test,expect} from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { ProductPage } from '../pages/ProductPage';
-import { CartPage } from '../pages/CartPage';
-import {testUser,generateUniqueEmail} from '../utils/testData';
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductPage } from "../pages/ProductPage";
+import { CartPage } from "../pages/CartPage";
+import { testUser, generateUniqueEmail } from "../utils/testData";
+import { ProductDetailsPage } from "../pages/ProductDetailsPage";
 
-test('TC20: Search Product, add to cart, verify cart after login',async({page})=>{
+test("TC20: Search Product, add to cart, verify cart after login", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  const productPage = new ProductPage(page);
+  const cartPage = new CartPage(page);
+  const email = generateUniqueEmail();
 
-    const loginPage = new LoginPage(page);
-    const productPage = new ProductPage(page)
-    const cartPage = new CartPage(page);
-    const email = generateUniqueEmail();
+  await productPage.open();
+  await productPage.searchProduct("Top");
+  await productPage.verifySearchResultVisible();
 
-    await productPage.open();
-    await productPage.searchProduct('Top');
-    await productPage.verifySearchResultVisible();
+  // Add first searched product to cart
+  await page.waitForLoadState("networkidle");
+  await productPage.hoverAndAddToCart(0);
+  await productPage.continueShopping();
 
-    // Add first searched product to cart
-    await page.waitForLoadState('networkidle');
-    await productPage.hoverAndAddToCart(0);
-    await productPage.continueShopping();
+  // View cart before login
+  await cartPage.open();
+  await cartPage.verifyCartPageDisplayed();
+  await cartPage.verifyProductCount(1);
 
-    // View cart before login
-    await cartPage.open();
-    await cartPage.verifyCartPageDisplayed();
-    await cartPage.verifyProductCount(1);
+  // Register and login
 
-    // Register and login 
+  await loginPage.open();
+  await loginPage.fillSignUpNameAndEmail(testUser.name, email);
+  await loginPage.fillAccountInformation(
+    testUser.title,
+    testUser.name,
+    testUser.password,
+    testUser.dob,
+  );
+  await loginPage.fillAddressInformation(
+    testUser.firstName,
+    testUser.lastName,
+    testUser.company,
+    testUser.address1,
+    testUser.address2,
+    testUser.country,
+    testUser.state,
+    testUser.city,
+    testUser.zipcode,
+    testUser.mobile,
+  );
+  await loginPage.verifyAccountCreated();
 
-    await loginPage.open();
-     await loginPage.fillSignUpNameAndEmail(testUser.name, email);
+  // Return to cart - product should still be there
+  await cartPage.open();
+  await cartPage.verifyCartPageDisplayed();
+  await cartPage.verifyProductCount(1);
+
+  await loginPage.deleteAccount();
+});
+
+test.describe("TC21 - Add Product review", () => {
+  const reviewDatasets = [
+    {
+      name: "Review One",
+      email: "rev1@test.com",
+      review: "EXcellent Quality Product",
+    },
+    {
+      name: "Reviewer Two",
+      email: "rev2@test.com",
+      review: "Good value for money, satisfied with purchase.",
+    },
+    {
+      name: "Reviewer Three",
+      email: "rev3@test.com",
+      review: "Average product, met basic expectations.",
+    },
+  ];
+
+  for (let dataset of reviewDatasets) {
+    test(`${dataset.name} `, async ({ page }) => {
+      const { name, email, review } = dataset;
+      const loginPage = new LoginPage(page);
+      const productPage = new ProductPage(page);
+      const detailPage = new ProductDetailsPage(page);
+      const dummyEmail = generateUniqueEmail();
+
+      await loginPage.open();
+      await loginPage.fillSignUpNameAndEmail(testUser.name, dummyEmail);
       await loginPage.fillAccountInformation(
         testUser.title,
         testUser.name,
@@ -49,10 +109,12 @@ test('TC20: Search Product, add to cart, verify cart after login',async({page})=
       );
       await loginPage.verifyAccountCreated();
 
-      // Return to cart - product should still be there
-      await cartPage.open();
-      await cartPage.verifyCartPageDisplayed();
-      await cartPage.verifyProductCount(1);
+      await productPage.open();
+      await productPage.verifyAllProductsVisible();
+      await productPage.clickViewProduct();
 
-      await loginPage.deleteAccount();
-})
+      await detailPage.fillreview(name, email, review);
+    });
+  }
+});
+
