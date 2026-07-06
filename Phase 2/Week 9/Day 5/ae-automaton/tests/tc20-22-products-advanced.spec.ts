f885ac40f11637e56d9eacf692bd1f9250f9bc77
Bedi Gupta <bedigupta76@gmail.com>
2026-07-07 01:57:56 +0530

    TC22 Completed and passed

--- Phase 2/Week 9/Day 5/ae-automaton/tests/tc20-22-products-advanced.spec.ts
+++ Phase 2/Week 9/Day 5/ae-automaton/tests/tc20-22-products-advanced.spec.ts
@@ -118,3 +118,55 @@ test.describe("TC21 - Add Product review", () => {
   }
 });
 
+// TC22 - Add to cart from Recommended Items
+
+test("TC22: - Add to cart from Recommended Items", async ({ page }) => {
+  const homePage = new HomePage(page);
+  const loginPage = new LoginPage(page);
+  const cartPage = new CartPage(page);
+  const email = generateUniqueEmail();
+
+  await loginPage.open();
+  await loginPage.fillSignUpNameAndEmail(testUser.name, email);
+  await loginPage.fillAccountInformation(
+    testUser.title,
+    testUser.name,
+    testUser.password,
+    testUser.dob,
+  );
+  await loginPage.fillAddressInformation(
+    testUser.firstName,
+    testUser.lastName,
+    testUser.company,
+    testUser.address1,
+    testUser.address2,
+    testUser.country,
+    testUser.state,
+    testUser.city,
+    testUser.zipcode,
+    testUser.mobile,
+  );
+  await loginPage.verifyAccountCreated();
+
+  await homePage.open();
+
+  //Scroll to recommended Items section at the bottom
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+  await expect(page.getByText(/recommended items/i)).toBeVisible({
+    timeout: 10_000,
+  });
+
+  // Target the active slide inside the recommended items carousel
+  const activeSlide = page.locator(".recommended_items .item.active");
+
+  // Find the first Add to cart button specifically inside that visible slide
+  const firstVisibleAddToCartBtn = activeSlide.locator(".add-to-cart").first();
+
+  // Click it
+  await firstVisibleAddToCartBtn.click();
+
+  // Go to cart
+  await cartPage.open();
+  await cartPage.verifyCartPageDisplayed();
+  await cartPage.verifyProductCount(1);
+});
